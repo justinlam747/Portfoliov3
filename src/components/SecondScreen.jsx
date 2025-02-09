08c848de133cd552f1348637efc64e85d3786fd8
justin <113154738+justinlam6@users.noreply.github.com>
2025-02-08 22:04:40 -0500

    Update SecondScreen.jsx

--- src/components/SecondScreen.jsx
+++ src/components/SecondScreen.jsx
@@ -8,7 +8,7 @@ import {
 } from "react-icons/fa";
 import { FiSearch } from "react-icons/fi";
 
-import photoID from "../assets/projects/edgeHacks.png";
+import photoID from "../assets/projects/photoID.png";
 
 const SecondScreen = () => {
   return (
@@ -33,7 +33,7 @@ const SecondScreen = () => {
               </div>
             </div>
 
-            <div className="hidden lg:flex items-center gap-4 mr-28  text-wrap tracking-wide">
+            <div className="hidden lg:flex items-center gap-4 mr-28 text-wrap tracking-wide">
               <div className="flex flex-col items-center text-gray-600 gap-1">
                 <FaHome className="text-lg" />
                 <span className="text-xs">Home</span>
@@ -60,14 +60,10 @@ const SecondScreen = () => {
           <div className="flex flex-col justify-start mt-10 lg:justify-start">
             <div className="bg-slate-300 w-full sm:w-3/5 md:w-3/5 lg:w-3/5 mx-auto rounded-t-lg rounded-b-lg border border-gray-300 shadow-lg">
               <div
-                className="  bg-white transform translate-y-24 translate-x-5 rounded-full border-4 border-gray-300 shadow-md w-60 h-60 "
+                className="bg-white transform translate-y-24 translate-x-5 rounded-full border-4 border-gray-300 shadow-md w-60 h-60"
                 style={{ zIndex: 10 }}
               >
-                <img
-                  src="./src/assets/projects/timer1.png"
-                  alt="Profile"
-                  className="rounded-full p-1"
-                />
+                <img src={photoID} alt="Profile" className="rounded-full p-1" />
               </div>
 
               <div className="bg-slate-200 p-6 flex flex-col items-start">
@@ -83,6 +79,7 @@ const SecondScreen = () => {
                     className="bg-blue-700 text-white px-4 py-2 rounded-xl"
                     href="https://www.linkedin.com/in/justinlam747/"
                     target="_blank"
+                    rel="noopener noreferrer"
                   >
                     Accept
                   </a>
@@ -91,7 +88,7 @@ const SecondScreen = () => {
                   </button>
                 </div>
 
-                <p className="text-gray-600 text-2xl mt-4 font-semibold  text-start">
+                <p className="text-gray-600 text-2xl mt-4 font-semibold text-start">
                   Hey! I'm Justin, an enthusiastic engineering student with a
                   passion for software. Let's connect!
                 </p>
